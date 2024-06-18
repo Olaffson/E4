44769f5090302a61eacd4ba2b53945dd6f29690a
Olaffson <olivierkotwica@gmail.com>
2024-06-18 15:44:23 +0200

    fix bug

--- app/appweb/appweb/settings.py
+++ app/appweb/appweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1$rzzo!j)jqg=4k_vj#rrq8p@v2va(o+4)q@dg(^qo5h7r%=^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','localhost','0.0.0.0']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appweb',
 ]
 
 MIDDLEWARE = [
