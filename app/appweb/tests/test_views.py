2fef376e5e5c34c820f5d9e29c5e637dff2c975d
Olaffson <olivierkotwica@gmail.com>
2024-07-16 11:31:31 +0200

    fix tests

--- app/appweb/tests/test_views.py
+++ app/appweb/tests/test_views.py
@@ -4,25 +4,19 @@ from django.test import Client
 from django.contrib.auth.models import User
 
 
-# @pytest.fixture
-# def client():
-#     return Client()
-
-
-# def test_home(client):
-#     url = reverse('home')
-#     response = client.get(url)
-#     assert response.status_code == 200
-#     assert b"AppWeb E4" in response.content
-
-
 @pytest.mark.django_db
 def test_home(client):
     # Créer un utilisateur de test
-    user = User.objects.create_user(username='testuser', password='testpassword')
+    user = User.objects.create_user(
+        username='testuser',
+        password='testpassword'
+    )
 
     # Se connecter avec l'utilisateur de test
-    client.login(username='testuser', password='testpassword')
+    client.login(
+        username='testuser',
+        password='testpassword'
+    )
 
     url = reverse('home')
     response = client.get(url)
